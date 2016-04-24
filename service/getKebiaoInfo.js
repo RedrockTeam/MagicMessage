855ddb640ae6cf92de9fbf4116b66c778395fdfb
Ling <wssgcg1213@qq.com>
2016-04-25 00:50:02 +0800

    fix

--- service/getKebiaoInfo.js
+++ service/getKebiaoInfo.js
@@ -8,32 +8,31 @@ import modelCache from '../models/cache';
 
 export default async function(openid) {
   const url = `http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/CourseTable/CourseTable/json/openid/${openid}/token/gh_68f0a1ffc303.html`;
-  // const kbInDb = modelCache.findOne({where: {
-  //   type: 'kebiao',
-  //   key: openid
-  // }});
-  // if (kbInDb && parseInt(kbInDb['expire']) > +new Date) {
-  //   return kbInDb['data'];
-  // }
+  const kbInDb = modelCache.findOne({where: {
+    type: 'kebiao',
+    key: openid
+  }});
+  if (kbInDb && parseInt(kbInDb['expire']) > +new Date) {
+    return kbInDb['data'];
+  }
 
   const remoteStr = await request(url);
   const remote = JSON.parse(remoteStr);
-  return remote['data'];
-  // if (remote && remote['info'] === 'success') {
-  //   const val = {
-  //     type: 'kebiao',
-  //     key: openid,
-  //     expire: +new Date + 7 * 24 * 3600 * 1000,
-  //     data: remote['data']
-  //   };
-  //   modelCache.findOrCreate({
-  //     where: {type: 'kebiao', key: openid},
-  //     defaults: val
-  //   }).spread(async (kb, created) => {
-  //     if (!created) {
-  //       await kb.update(val);
-  //     }
-  //   });
-  //   return remote['data'];
-  // }
+  if (remote && remote['info'] === 'success') {
+    const val = {
+      type: 'kebiao',
+      key: openid,
+      expire: +new Date + 7 * 24 * 3600 * 1000,
+      data: remote['data']
+    };
+    modelCache.findOrCreate({
+      where: {type: 'kebiao', key: openid},
+      defaults: val
+    }).spread(async (kb, created) => {
+      if (!created) {
+        await kb.update(val);
+      }
+    });
+    return remote['data'];
+  }
 }
